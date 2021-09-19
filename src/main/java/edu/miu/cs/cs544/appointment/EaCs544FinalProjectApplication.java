201c7bf31192f913852cb344d50691d563a9c443
Salah Khdairat <salah.khdairat@kaitsolutions.com>
2021-09-18 22:42:42 -0500

    init

---
+++ src/main/java/edu/miu/cs/cs544/appointment/EaCs544FinalProjectApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs544.appointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaCs544FinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EaCs544FinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/miu/cs/cs544/appointment/EaCs544FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.cs.cs544.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaCs544FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
