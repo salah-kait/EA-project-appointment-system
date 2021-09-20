438f3d63798a2bddd700aa1dd91b3c98aecbfee6
Salah Khdairat <salah.khdairat@kaitsolutions.com>
2021-09-20 02:18:22 -0500

    cicd

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ubuntu/server
-sudo java -jar -Dserver.port=80 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+sudo java -jar -Dspring.profiles.active=prod *.jar > /dev/null 2> /dev/null < /dev/null &
