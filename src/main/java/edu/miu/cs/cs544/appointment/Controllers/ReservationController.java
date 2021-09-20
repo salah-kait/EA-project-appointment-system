e5f661b72e6bb2e56c037341e5bf1a148a38f3f4
Alehegn <amdagnachew@gmail.com>
2021-09-20 11:11:28 -0500

    reservation controller

--- src/main/java/edu/miu/cs/cs544/appointment/Controllers/ReservationController.java
+++ src/main/java/edu/miu/cs/cs544/appointment/Controllers/ReservationController.java
@@ -1,13 +1,25 @@
 package edu.miu.cs.cs544.appointment.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import edu.miu.cs.cs544.appointment.Models.Reservation;
+import edu.miu.cs.cs544.appointment.Services.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
-
+@RequestMapping("api/reservation")
 public class ReservationController{
 
-    // Create user activation
+    @Autowired
+    private ReservationService reservationService;
 
+    // Create user activation
+    @PostMapping("/reserve")
+    public ResponseEntity<?> createReservation(@RequestParam(name = "appointment_id", required = true) Long id,
+                                               @RequestBody Reservation reservation){
+
+    }
 
 
     // Get list of reservation(Paginated)
