2bc3ef9a486553894622c15c434e158fb7e4ead0
Salah Khdairat <salah.khdairat@kaitsolutions.com>
2021-09-20 00:49:33 -0500

    cicd

--- src/main/java/edu/miu/cs/cs544/appointment/Models/Role.java
+++ src/main/java/edu/miu/cs/cs544/appointment/Models/Role.java
@@ -1,6 +1,8 @@
 package edu.miu.cs.cs544.appointment.Models;
 
 import edu.miu.cs.cs544.appointment.Models.enums.RoleName;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.NaturalId;
 
 import javax.persistence.*;
@@ -8,6 +10,8 @@ import javax.persistence.*;
 
 @Entity
 @Table(name = "roles")
+@Data
+@NoArgsConstructor
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,30 +21,5 @@ public class Role {
     @NaturalId
     @Column(length = 60)
     private RoleName name;
-
-    public Role() {
-    }
-
-
-
-    public Role(RoleName name) {
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public RoleName getName() {
-        return name;
-    }
-
-    public void setName(RoleName name) {
-        this.name = name;
-    }
 
 }
