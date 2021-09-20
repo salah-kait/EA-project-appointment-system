4289ce0a5674e63b8f928d33a3268d5f0f1f78af
ermias111 <ermimichael@gmail.com>
2021-09-20 12:12:10 -0500

    add controller and service methods

--- src/main/java/edu/miu/cs/cs544/appointment/Controllers/ReservationController.java
+++ src/main/java/edu/miu/cs/cs544/appointment/Controllers/ReservationController.java
@@ -2,9 +2,16 @@ package edu.miu.cs.cs544.appointment.Controllers;
 
 import edu.miu.cs.cs544.appointment.Models.Reservation;
 import edu.miu.cs.cs544.appointment.Services.ReservationService;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.Servlet;
+import java.net.URI;
 
 
 @RestController
@@ -17,13 +24,30 @@ public class ReservationController{
     // Create user activation
     @PostMapping("/reserve")
     public ResponseEntity<?> createReservation(@RequestParam(name = "appointment_id", required = true) Long id,
-                                               @RequestBody Reservation reservation){
-
+                                               @RequestBody Reservation reservation) throws NotFoundException {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/reservation/reserve").toUriString());
+        return ResponseEntity.created(uri).body(reservationService.createReservation(reservation, id));
     }
 
 
     // Get list of reservation(Paginated)
+    @GetMapping(path = "reservations", params = "paged = true")
+    public Page<Reservation> getReservations(Pageable pageable){
+        return reservationService.getAllReservations(pageable);
+    }
+
 
     // Update reservation
+    @PutMapping("{id}")
+    public ResponseEntity<Reservation> updateReservation(Long id, Reservation reservation){
+        Reservation result = reservationService.updateReservation(id, reservation);
+
+        if(result != null){
+            return ResponseEntity.ok(result);
+        }else{
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
 
 }
--- src/main/java/edu/miu/cs/cs544/appointment/Services/ReservationService.java
+++ src/main/java/edu/miu/cs/cs544/appointment/Services/ReservationService.java
@@ -5,11 +5,13 @@ import edu.miu.cs.cs544.appointment.Models.Reservation;
 import edu.miu.cs.cs544.appointment.Models.enums.ReservationStatus;
 import edu.miu.cs.cs544.appointment.Repositories.AppointmentRepository;
 import edu.miu.cs.cs544.appointment.Repositories.ReservationRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -25,42 +27,42 @@ public class ReservationService {
         this.appointmentRepository = appointmentRepository;
     }
 
-    public Reservation createReservation(Reservation reservation,Long id){
+    public Reservation createReservation(Reservation reservation,Long id) throws NotFoundException {
 
-        Optional<Appointment> appointment = appointmentRepository.findById(id);
-        if(appointment == null)
-            return null;
-        else {
-            Appointment appointment1 = appointment.get();
-            reservation.setAppointment(appointment1);
-            reservation.setStatus(ReservationStatus.PENDING);
-            return reservationRepository.save(reservation);
-        }
+        Appointment appointment = appointmentRepository.findById(id).orElseThrow(() ->
+                new NotFoundException("appointment not found")
+        );
+
+        reservation.setAppointment(appointment);
+        reservation.setStatus(ReservationStatus.PENDING);
+        return reservationRepository.save(reservation);
+
 
     }
 
-    public List<Reservation> getAllReservations(){
-        return reservationRepository.findAll();
+    public Page<Reservation> getAllReservations(Pageable pageable){
+        return reservationRepository.findAll(pageable);
     }
 
     public Reservation updateReservation(Long id, Reservation reservation){
         Reservation reservation1 = null;
-        if(reservationExist(id) != null){
-            reservation1 = reservationExist(id);
+        if(getReservation(id) != null){
+            reservation1 = getReservation(id);
 
             reservation1.setAppointment(reservation.getAppointment());
             reservation1.setReservationDateTime(reservation.getReservationDateTime());
-            reservation1.setStatus(ReservationStatus.PENDING);
+//            reservation1.setStatus(ReservationStatus.PENDING);
 
-            reservationRepository.save(reservation1);
+            reservationRepository.save(reservation);
 
             return reservation1;
+
         } else
         return reservation1;
     }
 
     public Reservation cancelReservation(Reservation reservation, Long id){
-        if(reservationExist(id) != null){
+        if(getReservation(id) != null){
             reservation.setStatus(ReservationStatus.CANCELED);
             reservationRepository.save(reservation);
             return reservation;
@@ -68,7 +70,7 @@ public class ReservationService {
         return null;
     }
 
-    public Reservation reservationExist(Long id){
+    public Reservation getReservation(Long id){
         Optional<Reservation> reservation = reservationRepository.findById(id);
 
         if(reservation.isPresent())
