aaa25f2c472980feb7971c80a864fe7a0d776a6b
Salah Khdairat <salah.khdairat@kaitsolutions.com>
2021-09-21 18:13:25 -0500

    fixes

--- src/main/java/edu/miu/cs/cs544/appointment/Util/Email/EmailQueueDTO.java
+++ src/main/java/edu/miu/cs/cs544/appointment/Util/Email/EmailQueueDTO.java
@@ -4,9 +4,14 @@ import edu.miu.cs.cs544.appointment.Util.SQS.IQueueMessageDTO;
 import lombok.Data;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 
 @Data
+@Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class EmailQueueDTO implements IQueueMessageDTO<MailMessage> {
 
     @Value("${app.sqs.queue.messaging}")
@@ -15,10 +20,6 @@ public class EmailQueueDTO implements IQueueMessageDTO<MailMessage> {
     private MailMessage mailMessage;
     private String body;
     private String to;
-
-    public EmailQueueDTO(MailMessage mailMessage) {
-        this.mailMessage = mailMessage;
-    }
 
     @Override
     public String getQueueName() {
--- src/main/java/edu/miu/cs/cs544/appointment/Util/SQS/SqsMessageProducer.java
+++ src/main/java/edu/miu/cs/cs544/appointment/Util/SQS/SqsMessageProducer.java
@@ -1,6 +1,5 @@
 package edu.miu.cs.cs544.appointment.Util.SQS;
 
-import edu.miu.cs.cs544.appointment.Util.Email.MailMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
@@ -19,25 +18,25 @@ public class SqsMessageProducer {
         this.queueMessagingTemplate = queueMessagingTemplate;
     }
 
-    public <T> void send(T message, Map<String, Object> headers) {
+    public <T> void send(IQueueMessageDTO message, Map<String, Object> headers) {
         if (message == null) {
             log.warn("SQS Producer cant produce 'null' value");
             return;
         }
 
-        //log.debug(" Messgae {} " + message);
-        //log.debug(" Queue name {} " + message.getQueueName());
-        queueMessagingTemplate.convertAndSend("AppointmentSystemQueueStack-QuoteQueue-VLCL6HSTVQCE", message,headers);
+        log.debug(" Messgae {} " + message);
+        log.debug(" Queue name {} " + message.getQueueName());
+        queueMessagingTemplate.convertAndSend(message.getQueueName(), message.getMessage(),headers);
     }
-    public <T> void send(T message) {
+    public <T> void send(IQueueMessageDTO message) {
         if (message == null) {
             log.warn("SQS Producer cant produce 'null' value");
             return;
         }
 
-//        log.debug(" Messgae {} " + message.getMessage());
-     //  log.debug(" Queue name {} " + message.getQueueName());
-        queueMessagingTemplate.convertAndSend("AppointmentSystemQueueStack-QuoteQueue-VLCL6HSTVQCE",new MailMessage("this is subject","this is bodey","salah.khudairat@gmail.com"));
+       log.debug(" Messgae {} " + message.getMessage());
+       log.debug(" Queue name {} " + message.getQueueName());
+        queueMessagingTemplate.convertAndSend(message.getQueueName(),message.getMessage());
     }
 
 }
