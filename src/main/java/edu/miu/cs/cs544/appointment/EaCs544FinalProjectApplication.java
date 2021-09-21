5251141cc298bfff44353f328947bb60f273b08a
Salah Khdairat <salah.khdairat@kaitsolutions.com>
2021-09-20 19:53:23 -0500

    swageer and health check

--- src/main/java/edu/miu/cs/cs544/appointment/EaCs544FinalProjectApplication.java
+++ src/main/java/edu/miu/cs/cs544/appointment/EaCs544FinalProjectApplication.java
@@ -2,8 +2,10 @@ package edu.miu.cs.cs544.appointment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EaCs544FinalProjectApplication {
 
     public static void main(String[] args) {
