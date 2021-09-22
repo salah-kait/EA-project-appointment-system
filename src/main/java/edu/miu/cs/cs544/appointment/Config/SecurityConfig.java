5890e32435fdfda11946f8a7a0dfb1eb059efecc
Salah Khdairat <salah.khdairat@kaitsolutions.com>
2021-09-22 00:20:54 -0500

    fixes

--- src/main/java/edu/miu/cs/cs544/appointment/Config/SecurityConfig.java
+++ src/main/java/edu/miu/cs/cs544/appointment/Config/SecurityConfig.java
@@ -81,10 +81,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers("/api/auth/**")
                 .permitAll()
-                .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability")
-                .permitAll()
-                .antMatchers(HttpMethod.GET, "/api/users/**")
-                .permitAll()
                 .anyRequest()
                 .authenticated();
 
